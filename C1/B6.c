6b774bc97b225558ee234c7a2fa4b5a114563e82
Đinh Việt Hoàng <101950801+VietHoangg03@users.noreply.github.com>
2023-02-10 00:45:35 +0700

    Create B6.c

---
+++ C1/B6.c
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int n;
+   double sum = 0.0;
+   cout <<"Enter a positive integer: "; //nhap so nguyen duong
+   cin >> n;
+   for (int i=1 ; i<=n; i++) {
+       sum +=1.0/i*(i+1);
+   }
+   
+   cout << "Sum = "<< sum;
+   return 0;
+}
